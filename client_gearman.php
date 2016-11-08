0c69ca73c0f978ddc8c78655b6ff6806f31de919
Webster <lebronwebz@gmail.com>
2016-11-08 23:42:00 +0700

    test

--- client_gearman.php
+++ client_gearman.php
@@ -1,5 +1,5 @@
 <?php
 $client= new GearmanClient();
-$client->addServer();
+$client->addServer('127.0.0.1');
 print $client->do("reverse", "Hello World!");
 ?>
--- validate_time.php
+++ validate_time.php
@@ -1,39 +1,11 @@
 <?php
 
-echo "Starting\n";
+$worker= new GearmanWorker();
+$worker->addServer('127.0.0.1');
+$worker->addFunction("reverse", "my_reverse_function");
+while ($worker->work());
 
-# Create our worker object.
-$gmworker= new GearmanWorker();
-
-# Add default server (localhost).
-$gmworker->addServer();
-
-# Register function "reverse" with the server.
-$gmworker->addFunction("reverse", "reverse_fn");
-
-# Set the timeout to 5 seconds
-$gmworker->setTimeout(5000);
-
-echo "Waiting for job...\n";
-while(@$gmworker->work() || $gmworker->returnCode() == GEARMAN_TIMEOUT)
-{
-  if ($gmworker->returnCode() == GEARMAN_TIMEOUT)
-  {
-    # Normally one would want to do something useful here ...
-    echo "Timeout. Waiting for next job...\n";
-    continue;
-  }
-
-  if ($gmworker->returnCode() != GEARMAN_SUCCESS)
-  {
-    echo "return_code: " . $gmworker->returnCode() . "\n";
-    break;
-  }
-}
-
-echo "Done\n";
-
-function reverse_fn($job)
+function my_reverse_function($job)
 {
   return strrev($job->workload());
 }
